72accb998a295e1adc110ac46b823edec69c7340
chasejkal <chasejkal@gmail.com>
2022-01-26 17:24:43 -0600

    Update utility.py

--- utility/utility.py
+++ utility/utility.py
@@ -6,14 +6,12 @@ class UtilityExamples(commands.Cog):
     """Provides basic utility commands"""
     def __init__(self, bot):
         self.bot = bot
-        self.db = bot.plugin_db.get_partition(self)
 
 
     @group.command()
     async def get(self, ctx, member: discord.Member = None):
         """Gets a user's group"""
         member = member or ctx.author
-        data = await self.db.find_one({'user_id': str(member.id)})
         role = get(member.server.roles, name="Media")
         await bot.add_roles(member, role) 
 
