90e31bf3a8295186e02819220cc1cc40550119b3
chasejkal <chasejkal@gmail.com>
2022-02-01 22:40:53 -0600

    Update claim.py

--- claim/claim.py
+++ claim/claim.py
@@ -10,11 +10,6 @@ class ClaimThread(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.db = self.bot.plugin_db.get_partition(self)
-        check_reply.fail_msg = 'This thread has been claimed by another user.'
-        self.bot.get_command('reply').add_check(check_reply)
-        self.bot.get_command('areply').add_check(check_reply)
-        self.bot.get_command('fareply').add_check(check_reply)
-        self.bot.get_command('freply').add_check(check_reply)
 
     @checks.has_permissions(PermissionLevel.SUPPORTER)
     @checks.thread_only()
@@ -81,11 +76,6 @@ class ClaimThread(commands.Cog):
         await ctx.invoke(self.bot.get_command('reply'), msg=msg)
 
 
-async def check_reply(ctx):
-    thread = await ctx.bot.get_cog('ClaimThread').db.find_one({'thread_id': str(ctx.thread.channel.id)})
-    if thread:
-        return ctx.author.bot or str(ctx.author.id) in thread['claimers']
-    return True
 
 
 def setup(bot):
