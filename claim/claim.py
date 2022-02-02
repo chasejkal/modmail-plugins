ce631761c0cc9003e0c7666d01309c0eb4747fb5
chasejkal <chasejkal@gmail.com>
2022-02-02 13:35:02 -0600

    Update claim.py

--- claim/claim.py
+++ claim/claim.py
@@ -18,12 +18,12 @@ class ClaimThread(commands.Cog):
         thread = await self.db.find_one({'thread_id': str(ctx.thread.channel.id)})
         if thread is None:
             await self.db.insert_one({'thread_id': str(ctx.thread.channel.id), 'claimers': [str(ctx.author.id)]})
+            await ctx.channel.set_permissions(ctx.message.author, view_channel=True)
             await ctx.send(f'Thread claimed by {ctx.message.author}, use `;addstaff (userid)` if you require assistance from other staff members.')
             helper = discord.utils.get(ctx.guild.roles, name="Helper")
             mod = discord.utils.get(ctx.guild.roles, name="yo can i get mod?")
             await ctx.channel.set_permissions(helper, view_channel=False)
             await ctx.channel.set_permissions(mod, view_channel=False)
-            await ctx.channel.set_permissions(ctx.message.author, view_channel=True)
         else:
             await ctx.send('Thread is already claimed')
 
@@ -48,32 +48,6 @@ class ClaimThread(commands.Cog):
             await self.db.find_one_and_update({'thread_id': str(ctx.thread.channel.id)}, {'$pull': {'claimers': str(member.id)}})
             await ctx.send('Removed from claimers')
 
-    @checks.has_permissions(PermissionLevel.SUPPORTER)
-    @checks.thread_only()
-    @commands.command()
-    async def transferclaim(self, ctx, *, member: discord.Member):
-        """Removes all users from claimers and gives another member all control over thread"""
-        thread = await self.db.find_one({'thread_id': str(ctx.thread.channel.id)})
-        if thread and str(ctx.author.id) in thread['claimers']:
-            await self.db.find_one_and_update({'thread_id': str(ctx.thread.channel.id)}, {'$set': {'claimers': [str(member.id)]}})
-            await ctx.send('Added to claimers')
-
-    @checks.has_permissions(PermissionLevel.MODERATOR)
-    @checks.thread_only()
-    @commands.command()
-    async def overrideaddclaim(self, ctx, *, member: discord.Member):
-        """Allow mods to bypass claim thread check in add"""
-        thread = await self.db.find_one({'thread_id': str(ctx.thread.channel.id)})
-        if thread:
-            await self.db.find_one_and_update({'thread_id': str(ctx.thread.channel.id)}, {'$addToSet': {'claimers': str(member.id)}})
-            await ctx.send('Added to claimers')
-
-    @checks.has_permissions(PermissionLevel.MODERATOR)
-    @checks.thread_only()
-    @commands.command()
-    async def overridereply(self, ctx, *, msg: str=""):
-        """Allow mods to bypass claim thread check in reply"""
-        await ctx.invoke(self.bot.get_command('reply'), msg=msg)
 
 
 
