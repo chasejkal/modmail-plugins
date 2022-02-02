c9db0ecc0ed45ff6eccc4d5470e7c24e8c4b1f53
chasejkal <chasejkal@gmail.com>
2022-02-01 20:38:03 -0600

    Update claim.py

--- claim/claim.py
+++ claim/claim.py
@@ -25,6 +25,7 @@ class ClaimThread(commands.Cog):
         if thread is None:
             await self.db.insert_one({'thread_id': str(ctx.thread.channel.id), 'claimers': [str(ctx.author.id)]})
             await ctx.send('Claimed')
+            await ctx.channel.set_permissions(ctx.guild.default_role, view_channel=False)
         else:
             await ctx.send('Thread is already claimed')
 
