71e76bd6b6fa8aa036edd8d79bcf0111aa6d4298
chasejkal <chasejkal@gmail.com>
2022-02-01 21:05:39 -0600

    Update claim.py

--- claim/claim.py
+++ claim/claim.py
@@ -22,10 +22,12 @@ class ClaimThread(commands.Cog):
     async def claim(self, ctx):
         thread = await self.db.find_one({'thread_id': str(ctx.thread.channel.id)})
         if thread is None:
-            await self.db.insert_one({'thread_id': str(ctx.thread.channel.id), 'claimers': [str(ctx.author.id)]})
-            await ctx.send('Claimed')
-        else:
-            await ctx.send('Thread is already claimed')
+            await ctx.send(f'Thread claimed by {ctx.message.author}, use `;adduser (userid)`  if you require assistance from other staff members.')
+            helper = discord.utils.get(ctx.guild.roles, name="Helper")
+            mod = discord.utils.get(ctx.guild.roles, name="yo can i get mod?")
+            await ctx.channel.set_permissions(helper, view_channel=False)
+            await ctx.channel.set_permissions(mod, view_channel=False)
+            await ctx.channel.set_permissions(ctx.message.author, view_channel=True)
 
     @checks.has_permissions(PermissionLevel.SUPPORTER)
     @checks.thread_only()
@@ -34,7 +36,7 @@ class ClaimThread(commands.Cog):
         """Adds another user to the thread claimers"""
         thread = await self.db.find_one({'thread_id': str(ctx.thread.channel.id)})
         if thread and str(ctx.author.id) in thread['claimers']:
-            await self.db.find_one_and_update({'thread_id': str(ctx.thread.channel.id)}, {'$addToSet': {'claimers': str(member.id)}})
+            await ctx.channel.set_permissions(member, view_channel=True)
             await ctx.send('Added to claimers')
 
     @checks.has_permissions(PermissionLevel.SUPPORTER)
@@ -42,44 +44,12 @@ class ClaimThread(commands.Cog):
     @commands.command()
     async def removeclaim(self, ctx, *, member: discord.Member):
         """Removes a user from the thread claimers"""
-        thread = await self.db.find_one({'thread_id': str(ctx.thread.channel.id)})
         if thread and str(ctx.author.id) in thread['claimers']:
-            await self.db.find_one_and_update({'thread_id': str(ctx.thread.channel.id)}, {'$pull': {'claimers': str(member.id)}})
-            await ctx.send('Removed from claimers')
-
-    @checks.has_permissions(PermissionLevel.SUPPORTER)
-    @checks.thread_only()
-    @commands.command()
-    async def transferclaim(self, ctx, *, member: discord.Member):
-        """Removes all users from claimers and gives another member all control over thread"""
-        thread = await self.db.find_one({'thread_id': str(ctx.thread.channel.id)})
-        if thread and str(ctx.author.id) in thread['claimers']:
-            await self.db.find_one_and_update({'thread_id': str(ctx.thread.channel.id)}, {'$set': {'claimers': [str(member.id)]}})
-            await ctx.send('Added to claimers')
-
-    @checks.has_permissions(PermissionLevel.MODERATOR)
-    @checks.thread_only()
-    @commands.command()
-    async def overrideaddclaim(self, ctx, *, member: discord.Member):
-        """Allow mods to bypass claim thread check in add"""
-        thread = await self.db.find_one({'thread_id': str(ctx.thread.channel.id)})
-        if thread:
-            await self.db.find_one_and_update({'thread_id': str(ctx.thread.channel.id)}, {'$addToSet': {'claimers': str(member.id)}})
-            await ctx.send('Added to claimers')
-
-    @checks.has_permissions(PermissionLevel.MODERATOR)
-    @checks.thread_only()
-    @commands.command()
-    async def overridereply(self, ctx, *, msg: str=""):
-        """Allow mods to bypass claim thread check in reply"""
-        await ctx.invoke(self.bot.get_command('reply'), msg=msg)
+            await ctx.channel.set_permissions(member, view_channel=True)
+            await ctx.send(f'Removed {member}')
 
 
-async def check_reply(ctx):
-    thread = await ctx.bot.get_cog('ClaimThread').db.find_one({'thread_id': str(ctx.thread.channel.id)})
-    if thread:
-        return ctx.author.bot or str(ctx.author.id) in thread['claimers']
-    return True
+
 
 
 def setup(bot):
