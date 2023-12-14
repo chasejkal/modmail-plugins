2c4c16af430839d3f93765cea70e753b79210c25
chasejkal <chasejkal@gmail.com>
2023-12-14 12:11:07 -0600

    Update claim.py

--- claim/claim.py
+++ claim/claim.py
@@ -1,21 +1,14 @@
-
 import discord
 from discord.ext import commands
-
 from core import checks
 from core.checks import PermissionLevel
 
-
 class ClaimThread(commands.Cog):
     """Allows supporters to claim thread by sending claim in the thread channel"""
+
     def __init__(self, bot):
         self.bot = bot
         self.db = self.bot.plugin_db.get_partition(self)
-        check_reply.fail_msg = 'This thread has been claimed by another user.'
-        self.bot.get_command('reply').add_check(check_reply)
-        self.bot.get_command('areply').add_check(check_reply)
-        self.bot.get_command('fareply').add_check(check_reply)
-        self.bot.get_command('freply').add_check(check_reply)
 
     @checks.has_permissions(PermissionLevel.SUPPORTER)
     @checks.thread_only()
@@ -76,11 +69,10 @@ class ClaimThread(commands.Cog):
         await ctx.invoke(self.bot.get_command('reply'), msg=msg)
 
 
-async def check_reply(ctx):
-    thread = await ctx.bot.get_cog('ClaimThread').db.find_one({'thread_id': str(ctx.thread.channel.id)})
-    if thread:
-        return ctx.author.bot or str(ctx.author.id) in thread['claimers']
-    return True
+def check_reply(ctx):
+    return ctx.author.bot or str(ctx.author.id) in ctx.thread.claimers if hasattr(ctx, 'thread') else False
+
+check_reply.fail_msg = 'This thread has been claimed by another user.'
 
 
 async def setup(bot):
